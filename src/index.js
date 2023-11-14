35fc60568b54cb1da1dc097c126d699d450de74a
youssefmoutahir <youssefmoutahir99@gmail.com>
2023-11-14 23:29:07 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+   
+<HashRouter>
 
     <App />
-    </HashRouter>
+</HashRouter>
+    
   </React.StrictMode>
 );
 
